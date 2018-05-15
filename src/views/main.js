5e7db2bfeaecab1c265bc9920aa331f5ef6e8b86
Axel Anderson <axel@ftml.net>
2018-05-14 21:50:50 -0400

    move event listener to h1

--- src/views/main.js
+++ src/views/main.js
@@ -4,9 +4,11 @@ const mainView = (state, emit) => {
   const onclick = () => emit('draw')
 
   return html`
-    <body class="sans-serif vh-100 w-100 ma0" onclick=${onclick}>
+    <body class="sans-serif vh-100 w-100 ma0">
       <div class="flex items-center justify-content vh-100">
-        <h1 class="f-headline-ns f1 tc w-100">${state.strategy}</h1>
+        <h1 class="f-headline-ns f1 tc w-100" onclick=${onclick}>${
+    state.strategy
+  }</h1>
       </div>
     </body>
   `
