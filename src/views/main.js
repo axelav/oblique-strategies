b62d1248c07efd5144e387d9e2de17f2a12b6bd8
Axel Anderson <axel@ftml.net>
2018-10-22 14:23:18 -0400

    move click handler to body; adjust styles

--- src/views/main.js
+++ src/views/main.js
@@ -1,14 +1,12 @@
 const html = require('choo/html')
 
 const mainView = (state, emit) => {
-  const onclick = () => emit('draw')
+  const handleClick = () => emit('draw')
 
   return html`
-    <body class="sans-serif vh-100 w-100 ma0">
+    <body class="sans-serif vh-100 w-100 ma0" onclick=${handleClick}>
       <div class="flex items-center justify-content vh-100">
-        <h1 class="f-headline-l f1 tc w-100" onclick=${onclick}>${
-    state.strategy
-  }</h1>
+        <h1 class="f-headline-l f1 tc w-100 ma0 ph4">${state.strategy}</h1>
       </div>
     </body>
   `
